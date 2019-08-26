99497753a8e5267f48997b28d7f4b0005ca72ab1
Alberto Masia <alberto.masia@gmail.com>
2019-08-26 16:28:11 +0200

    Add state to application
    Create AddFish form

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,23 @@ import Order from './Order'
 import Inventory from './Inventory'
 
 class App extends React.Component {
+
+  state = {
+    fishes: {},
+    orders: {}
+  }
+
+
+  addFish = (fish) => {
+    // update our state
+    const fishes = { ...this.state.fishes }
+    // add in our new fish
+    const timeStamp = Date.now()
+    fishes[`fish-${timeStamp}`] = fish
+    // set state
+    this.setState({ fishes })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +29,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
