692685c7372689a44ad32059e1f1786ba4b73472
Alberto Masia <alberto.masia@gmail.com>
2019-08-26 12:21:40 +0200

    Add routes and notFound component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './css/style.css'
-
 import App from './components/App'
 import StorePicker from './components/StorePicker'
+import NotFound from './components/NotFound'
 
-render(<App />, document.getElementById('main'))
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={StorePicker} />
+        <Route path="/store/:storeId" component={App} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+render(<Root />, document.getElementById('main'))
