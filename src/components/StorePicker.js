45dd1cd550b36128b93aebc94739f912f843ea56
Alberto Masia <alberto.masia@gmail.com>
2019-08-26 12:48:31 +0200

    Structure StorePicker component use ref to gat input value

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,34 @@
 import React from 'react'
 
+import { getFunName } from '../helpers'
+
 class StorePicker extends React.Component {
+  storeInput = React.createRef()
+
+  // constructor() {
+  //   super()
+  //   this.goToStore = this.goToStore.bind(this)
+  // }
+
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log(this.storeInput.current.value)
+    // first grub the text from the box
+    // then go to transition from / to /store/:storeId
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name" />
-        <button>Visit Store ➡</button>
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={this.storeInput}
+        />
+        <button>Visit Store</button>
       </form>
     )
   }
